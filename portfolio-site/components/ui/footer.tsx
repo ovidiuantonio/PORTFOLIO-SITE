722dd3709dc3d912ad13cc45d05cd9f46d2a4b5c
Matei Ovidiu <mateiovidiu10@gmail.com>
2025-02-12 00:32:19 +0200

    updated copyright year

--- portfolio-site/components/ui/footer.tsx
+++ portfolio-site/components/ui/footer.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <p className="footer font-body-['Poppins'] text-sm uppercase text-slate-200 text-center py-24">© 2024 SOUL REMESHER</p>
+        <p className="footer font-body-['Poppins'] text-sm uppercase text-slate-200 text-center py-24">© 2025 SOUL REMESHER</p>
     );
 }
