393aa4c7f17b349d15e3e00d6e20df0e33d67d85
Matei Ovidiu <mateiovidiu10@gmail.com>
2025-06-15 20:13:12 +0300

    yt video

--- portfolio-site/app/about/page.tsx
+++ portfolio-site/app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <section className="md:p-12 pt-0 px-0">
       <h1 className="text-4xl font-bold uppercase font-body-['Poppins'] text-center pt-16">About</h1>
-      <iframe className="my-12 md:w-[90%] w-full md:h-[50rem] h-[20rem] mx-auto md:mb-24 mb-12" src="https://youtu.be/IzcqeaGWoJQ" title="국립남도국악원 - Jindo National Gugak Center - 사믈놀이 공연" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe className="my-12 md:w-[90%] w-full md:h-[50rem] h-[20rem] mx-auto md:mb-24 mb-12" src="https://www.youtube.com/watch?v=IzcqeaGWoJQ" title="국립남도국악원 - Jindo National Gugak Center - 사믈놀이 공연" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       
       <p className="about-text md:text-md text-sm text-center mb-6 md:w-[50%] w-[90%] mx-auto font-body-['Poppins'] font-normal">
         Ovidiu-Antonio, the Soul Remesher, is a 3D Artist based in Romania who loves creating stylized artworks in Blender
