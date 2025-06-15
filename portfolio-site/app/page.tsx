cf5f09bbf4fad92bea51eb1b709b094e4538980a
Matei Ovidiu <mateiovidiu10@gmail.com>
2025-06-15 20:11:00 +0300

    finally fixed the scoll

--- portfolio-site/app/page.tsx
+++ portfolio-site/app/page.tsx
@@ -1,19 +1,80 @@
+"use client";
+
 export default function Home() {
   return (
-    <section className="md:p-12 pt-0 w-[100%] p-1">
+     <section className="md:p-12 pt-0 w-[100%] p-1">
       <h1 className="text-4xl font-bold uppercase font-body-['Poppins'] text-center pt-16">Soul Remesher</h1>
       <p className="text-lg font-thin font-body-['Poppins'] text-center pt-6 pb-24 tracking-wide text-gray-400">A 3D artist who loves creating meaningful and fun things.</p>
 
+      {/* We no longer need the wrapper divs, just the grid container */}
       <div className="projects grid grid-cols-1 md:grid-cols-3 gap-3 min-h-[30rem] align-center justify-start">
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/226768985?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/216179409?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/205240437?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/203708199?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/200664459?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/191810923?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
-        <iframe className="w-[100%] md:min-h-[30rem] min-h-[20rem]" src="https://www.behance.net/embed/project/180210327?ilo0=1" allowFullScreen loading="lazy" allow="clipboard-write"></iframe>
+
+        {/* The className is simplified and scrolling="no" is added */}
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0" 
+          src="https://www.behance.net/embed/project/226768985?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0" 
+          src="https://www.behance.net/embed/project/216179409?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+        
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0"
+          src="https://www.behance.net/embed/project/205240437?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0"
+          src="https://www.behance.net/embed/project/203708199?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0"
+          src="https://www.behance.net/embed/project/200664459?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0"
+          src="https://www.behance.net/embed/project/191810923?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+
+        <iframe 
+          className="iframe w-full md:min-h-[30rem] min-h-[20rem] border-0"
+          src="https://www.behance.net/embed/project/180210327?ilo0=1" 
+          allowFullScreen 
+          loading="lazy" 
+          allow="clipboard-write"
+          scrolling="no">
+        </iframe>
+
       </div>
-      
     </section>
   );
 }
+
